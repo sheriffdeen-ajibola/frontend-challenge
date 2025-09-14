ac4ce5bab9924ab6fcf69acbaf178d223d098015
sheriff <ajibolasheriffdeen02@gmail.com>
2025-09-14 18:05:04 +0100

    carouse card created

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="P-4 border-2 border-black w-full">THE NAVBAR</div>;
+  return <div className="P-4 border-2 border-black w-full p-4">THE NAVBAR</div>;
 };
 
 export default Navbar;
