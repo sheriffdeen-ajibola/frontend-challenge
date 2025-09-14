fc0835bdc5d95025b99acc66522a64c156566a46
sheriff <ajibolasheriffdeen02@gmail.com>
2025-09-14 18:37:56 +0100

    fixes

--- src/components/carouselCard.tsx
+++ src/components/carouselCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CarouselCard = () => {
-  return <div className="w-full "></div>;
+  return (
+    <div className="w-3/5 shadow-2xl mx-auto border m-8 max-w-7x ">
+      this is the carousel card
+    </div>
+  );
 };
 
 export default CarouselCard;
