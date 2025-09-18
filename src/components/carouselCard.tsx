000b15bc3903b76c3c9f3d788a39958170c15860
sheriff <ajibolasheriffdeen02@gmail.com>
2025-09-18 12:06:01 +0100

    carousel card in progress

--- src/components/carouselCard.tsx
+++ src/components/carouselCard.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const CarouselCard = () => {
   return (
-    <div className="w-full border m-8 max-w-2xl min-h-[60vh] rounded-3xl bg-blue-300"></div>
+    <div className="w-full border m-8 max-w-2xl min-h-[60vh] rounded-3xl bg-blue-300 relative">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[5em] font-extrabold text-white uppercase bg-blue-700">
+        wan 2.2
+      </h1>
+      <span className="absolute top-1/12 left-1/24  text-[.5em] text-white uppercase ">
+        new image model
+      </span>
+    </div>
   );
 };
 
