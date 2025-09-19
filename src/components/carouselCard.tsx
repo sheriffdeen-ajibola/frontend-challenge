05c0b4486099d9765d9c9fc8b8f488b12744ee5f
sheriff <ajibolasheriffdeen02@gmail.com>
2025-09-19 13:16:51 +0100

    fonts confiured

--- src/components/carouselCard.tsx
+++ src/components/carouselCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CarouselCard = () => {
   return (
     <div className="w-full border m-8 max-w-2xl min-h-[60vh] rounded-3xl bg-blue-300 relative">
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[5em] font-extrabold text-white uppercase bg-blue-700">
+      <h1 className="font-mont w-full absolute top-1/2 left-1/2 -translate-x-1/2 text-[5em] font-bold text-white uppercase bg-blue-700">
         wan 2.2
       </h1>
       <span className="absolute top-1/12 left-1/24  text-[.5em] text-white uppercase ">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat, Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
-import CarouselCard from "@/components/carouselCard";
 import CarouselContainer from "@/components/carouselContainer";
 
 const geistSans = Geist({
@@ -14,9 +12,23 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const montserrat = Montserrat({
+  variable: "--font-mont",
+  subsets: ["latin"],
+  weight: ["800"], // for WAN 2.2 heading
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  // 400 = body, 500 = button, 600 = subheading
+});
+
 export default function Home() {
   return (
-    <div className={`${geistSans.className} ${geistMono.className} `}>
+    <div
+      className={`${geistSans.className} ${geistMono.className} ${montserrat.className} ${inter.className} `}>
       <Navbar />
       <CarouselContainer />
     </div>
